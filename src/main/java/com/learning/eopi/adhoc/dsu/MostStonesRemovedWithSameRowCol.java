2e3ab2228a8490009c96054c0cbd5309cf9a84e3
Aman Garg <aman.g@flipkart.com>
2020-03-31 19:18:53 +0530

    Adding adhoc package

---
+++ src/main/java/com/learning/eopi/adhoc/dsu/MostStonesRemovedWithSameRowCol.java
@@ -0,0 +1,21 @@
+package com.learning.eopi.adhoc.dsu;
+
+/**
+ * On a 2D plane, we place stones at some integer coordinate points.  Each coordinate point may have at most one stone.
+ * <p>
+ * Now, a move consists of removing a stone that shares a column or row with another stone on the grid.
+ * <p>
+ * What is the largest possible number of moves we can make?
+ * <p>
+ * <a href="https://leetcode.com/problems/most-stogsnes-removed-with-same-row-or-column/>
+ */
+public class MostStonesRemovedWithSameRowCol {
+
+    public int removeStones(int[][] stones) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
